7caa534fc77400d438c71a1a8369f4c71419129f
Fortox <ori6g1@gmail.com>
2024-01-26 20:56:14 +0200

    added board logic

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,19 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+
+public class Board : MonoBehaviour //Responsible for word bank management and deciding which words to spawn
 {
     [Header("Attributes")]
-    private int wordAmount;
-    private Word[] wordsAtBoard;
-    private string[] wordbBank;
+    private int letterAmount;
+    [SerializeField] private int maxLettersAmount;
 
-   private void GetWordBank(string[] words)
+    private string[] wordBank;
+    private List<string> wordsOnBoard = new List<string>();
+
+    private WordSpawner wordSpawner;
+
+
+    private void Awake()
     {
-        wordbBank = words;
+        wordSpawner = GetComponent<WordSpawner>();
     }
+
+    private void SetWordBank(string[] words)
+    {
+        wordBank = words;
+    }
+
     private void SetWordsAtBoard()
     {
+        int allWordsLetterCount = checkLetterCount();
+        int wordsOnBoardLetterCount = 0;
+        int randomIndex;
+        List<int> usedIndexes = new List<int>();
 
+        if (allWordsLetterCount > maxLettersAmount)
+            letterAmount = maxLettersAmount;
+        else
+            letterAmount = allWordsLetterCount;
+
+        while (wordsOnBoardLetterCount < letterAmount)
+        {
+            randomIndex = Random.Range(0, (wordBank.Length - 1));
+            if (usedIndexes.Contains(randomIndex) || (wordsOnBoardLetterCount + wordBank[randomIndex].Length) > letterAmount)
+                continue;
+
+            usedIndexes.Add(randomIndex);
+            wordsOnBoard.Add(wordBank[randomIndex]);
+            wordsOnBoardLetterCount += wordBank[randomIndex].Length;
+        }
+    }
+
+    private int checkLetterCount()
+    {
+        string concatinatedWords = "";
+
+        for (int i = 0; i < wordBank.Length; i++)
+        {
+            concatinatedWords += wordBank[i];
+        }
+
+        return concatinatedWords.Length;
+    }
+
+    private void initializeBoards()
+    {
+        for (int i = 0; i < wordsOnBoard.Count; i++)
+            wordSpawner.SpawnWord(wordsOnBoard[i]);
     }
 }
