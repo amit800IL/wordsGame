8375fd670069ef002fa7b66a264efc3a1e7d4830
OmerZamir123 <62934105+OmerZamir123@users.noreply.github.com>
2024-01-26 16:34:28 +0200

    swivel with comments

--- Assets/Scripts/Swivel.cs
+++ Assets/Scripts/Swivel.cs
@@ -9,7 +9,16 @@ public class Swivel : MonoBehaviour
     [SerializeField] private float _swivelSpeed;
     [SerializeField] private float _swivelRange;
 
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
 
+    bool rotateRight = true;
+
+    void Start()
+    {
+        startRotation = transform.rotation;
+        targetRotation = Quaternion.Euler(0f, 0f, _swivelRange);
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,15 +27,51 @@ public class Swivel : MonoBehaviour
     }
     private void Swiveling()
     {
-        RotateTowards(_swivelRange);
+        float rotateAngle = rotateRight ? _swivelRange : -_swivelRange;
+        RotateTowards(rotateAngle);
+        float currentAngle = transform.rotation.z;
+        if (currentAngle >= targetRotation.z - 0.1)
+        {
+            rotateRight = false;
+        }
+        if (currentAngle <= startRotation.z + 0.1)
+        {
+            rotateRight = true;
+        }
     }
+
     private void RotateTowards(float rotationAngle)
     {
-    
-      transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(90, 0, rotationAngle), _swivelSpeed);
-        Debug.Log(transform.localRotation.eulerAngles.z) ; // fix rotation checker
-        //Debug.Log(transform.localEulerAngles.z);
-        // if (transform.localEulerAngles.z >= rotationAngle) return;
-        // transform.Rotate(0, 0, _swivelSpeed);
+        //float t = Mathf.PingPong(Time.time * rotationAngle * Time.deltaTime, 1f);
+        //if (!isForward)
+        //{
+        //    t = 1f - t; // Reverse the interpolation when going backward
+        //}
+
+        //transform.localRotation = Quaternion.Lerp(startRotation, targetRotation, t);
+
+        //// Check if the rotation reached the target, then toggle the direction
+        //if (t >= 0.99f)
+        //{
+        //    isForward = !isForward;
+        //}
+
+        //if (_swivelSpeed <= Mathf.Epsilon)
+        //{
+        //    return;
+        //}
+        //float cycles = Time.time / _swivelSpeed;
+        //float rawSinWave = Mathf.Sin(cycles * tau);
+        //float movementFactor = (rawSinWave + 1f) / 2f;
+        //transform.localRotation = Quaternion.Lerp(startingRotation, Quaternion.Euler(0, 0, rotationAngle * movementFactor), _swivelSpeed * timeCount);
+        //transform.position = startingRotation + offset;
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, 0, rotationAngle), _swivelSpeed);
+
+        //if (transform.localRotation.eulerAngles.z >= Quaternion.Euler(0, 0, rotationAngle).z)
+        //{
+        //    _swivelRange = -_swivelRange;
+        //    return;
+        //}
     }
 }
