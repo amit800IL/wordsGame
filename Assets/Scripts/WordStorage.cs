d704b1f137fc0673b30bd17aeda5631a7bcd6490
OmerZamir123 <62934105+OmerZamir123@users.noreply.github.com>
2024-01-26 11:21:55 +0200

    add word storage basic class layout

---
+++ Assets/Scripts/WordStorage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordStorage : MonoBehaviour
+{
+    [Header("Attributtes")]
+    private string[] _wordBank;
+
+    private void SetWordBank(string[] words)
+    {
+
+    }
+
+    private void SendWordBank(string[] words)
+    {
+
+    }
+}
